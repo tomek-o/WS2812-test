2637eee01b5b7073d3665684667bdd5327272209
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2026-09-19 17:37:26 +0200

    Update changelog

--- WS2812_test/_doc/changelog.h
+++ WS2812_test/_doc/changelog.h
@@ -4,5 +4,13 @@
 
 /** \page changelog Changelog
 
+Version 1.1
+- added WASAPI-based audio visualisation (VU-meter)
+	- works with Windows 7 and Windows 10
+	- captures either audio output (loopback) or input
+	- option to start visualisation automatically
+- added option to hide to tray
+- added option to add to autostart
+- updated to match latest turbocpp_template
 
 */
