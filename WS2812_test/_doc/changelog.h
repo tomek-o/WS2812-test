e4c3fb82bbcfe21aea2e6d8d6e3576fd8d17edeb
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2026-09-23 18:37:39 +0200

    Bump version number to 1.1.1, update changelog

--- WS2812_test/_doc/changelog.h
+++ WS2812_test/_doc/changelog.h
@@ -13,4 +13,9 @@ Version 1.1
 - added option to add to autostart
 - updated to match latest turbocpp_template
 
+Version 1.1.1
+- option to turn off LEDs on close (enabled by default)
+- audio visualisation
+	- changed minimum brightness to 2 - acceptable even in darkness but reducing color palette
+	- not updating bargraph in the GUI (intended mostly for debug purposes) if application is minimized or hidden to tray - reduced CPU usage down to ~0.2% on Haswell i5 laptop
 */
